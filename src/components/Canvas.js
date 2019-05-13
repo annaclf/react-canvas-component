cb1fd8f832beb59fc33a99e15290eafb96cbb825
Anna Fredriksson <leliloro@gmail.com>
2019-05-14 00:06:26 +0200

    Add - react canvas component

---
+++ src/components/Canvas.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+
+class Canvas extends Component {
+  state = {
+    isPainting: false,
+    mousePos: {
+      offsetX: 0, offsetY: 0,
+    },
+  }
+  hue = 0;
+  lineWidth = 1;
+
+  onMouseDown = ({nativeEvent}) => {
+    const {offsetX, offsetY} = nativeEvent;
+    this.setState({
+      isPainting: true,
+      mousePos: {offsetX, offsetY},
+    })
+  }
+
+  onMouseMove = ({nativeEvent}) => {
+    const {isPainting, mousePos} = this.state;
+    if(isPainting) {
+      const {offsetX, offsetY} = nativeEvent;
+      const offSetData = { offsetX, offsetY };
+      this.draw(mousePos, offSetData);
+    }
+  }
+
+  componentDidMount() {
+    // Here we set up the properties of the canvas element.
+    this.canvas.width = 300;
+    this.canvas.height = 300;
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.lineJoin = 'round';
+    this.ctx.lineCap = 'round';
+    this.ctx.lineWidth = 5;
+  }
+
+  endPaintEvent = ({nativeEvent}) => {
+    const {offsetX, offsetY} = nativeEvent;
+
+    const {isPainting} = this.state;
+    if (isPainting) {
+      this.setState({
+        isPainting: false,
+        mousePos: {
+          offsetX, offsetY
+        }
+      })
+    }
+  }
+
+  draw = (mousePos, currPos) => {
+    const { offsetX, offsetY } = currPos;
+    const { offsetX: x, offsetY: y } = mousePos;
+
+    this.ctx.strokeStyle = this.getColor();
+    this.ctx.lineWidth = this.lineWidth;
+    this.ctx.beginPath();
+    this.ctx.moveTo(x, y);
+    this.ctx.lineTo(offsetX, offsetY);
+    this.ctx.stroke();
+    this.setState({
+      mousePos: {offsetX, offsetY},
+    })
+  }
+
+  getColor() {
+    this.hue++;
+    if ( this.hue == 360 ) {
+    this.hue = 0;
+    }
+    return 'hsl(' + this.hue +', 100%, 50%)';
+    }
+
+
+  render() {
+    return (
+      <canvas
+        ref={(ref) => (this.canvas = ref)}
+        style={{ background: 'black' }}
+        onMouseDown={this.onMouseDown}
+        onMouseLeave={this.endPaintEvent}
+        onMouseUp={this.endPaintEvent}
+        onMouseMove={this.onMouseMove}
+        onTouchStart={this.onMouseDown}
+        onTouchEnd={this.endPaintEvent}
+        onTouchMove={this.onMouseMove}
+      />
+    );
+  }
+}
+
+export default Canvas;
